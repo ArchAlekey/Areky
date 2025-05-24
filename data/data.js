1a14de87e0d1f2c299d6e6102fe38f87baacb989
Alekey <alexmetal2001@gmail.com>
2025-05-24 01:07:27 -0600

    Up: Fase 1 del proyecto (Perfil Profesional)

--- data/data.js
+++ data/data.js
@@ -1,13 +1,38 @@
 
 export const habilidades = {
     lengaujes: {
-        "html": "Nive: 70%",
-        "css": "nivel: 50%",
-        "javascript": "nivel: 40%",
-        "mysql": "nivel: 70%"
+        "html": {lengauje: "HTML", nivel: "70%", habilidades: {
+            layout: "Creación de layouts y maquetación web.",
+            forms: "Creación de formularios.",
+            dialog: "Uso de cuadros de dialogo.",
+            table: "Creación y uso de tablas."
+        }},
+        "css": {lengauje: "CSS", nivel: "50%", habilidades: {
+            grid: "Conocimientos en CSS Grid para maquetado y diseño de interfaces web",
+            flex: "Conocimients en CSS Flex-Box para diseño de interfaces web"
+        }},
+        "javascript": {lengauje: "JS", nivel: "45%", habilidades:{
+            dom: "Conocimientos en el uso del árbol DOM.",
+            event: "Manejo de eventos del DOM.",
+            function: "Conocimientos en la creación y uso de funciones.",
+            object: "Creación y uso de objetos javascript."
+        }},
+        "mysql": {lengauje: "MYSQL", nivel: "70%", habilidades:{
+            dbStruct: "Conocimientos intermedios en creación de bases de datos.",
+            dbSPD: "Manejo intermedio de procedimientos almacenado.",
+            dbTransactional: "Uso de transacciónes para un mayor control en la manipulación de datos."
+        }}
     },
     herramientas: {
-        "git": "nivel: 50%",
-        "github": "nivel: 50%"
+        "git": {herramienta: "GIT", nivel: "50%", habilidades:{
+            init: "Creación y duplicación de repositorios",
+            basic: "Conocimientos generales en comandos basicos/escenciales de git.",
+            branch: "Uso de ramas para gestión de nuevas implementaciónes en el código.",
+            conflict: "Soluciones rapidas para conflictos entre repositorios."
+        }},
+        "github": {herramienta: "GITHUB", nivel: "50%", habilidades:{
+            repo: "Entendido del funcionamiento de repositorios en github.",
+            merge: "Gestión de pull request y merge dentro de algún proyecto."
+        }}
     }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,76 @@
 import { habilidades } from "../../data/data.js";
 
-const btnHtml = document.querySelector('#html');
+const btnIcon = document.querySelectorAll(".icon");
 const displayDialog = document.querySelector('.dialog-dinamic');
-const btnClose = document.querySelector('.btn-close');
 
-btnHtml.addEventListener("click", function(){
-    displayDialog.showModal();
+
+btnIcon.forEach((boton, i) =>{
+    boton.addEventListener("click", (event) => {
+        displayDialog.showModal();
+        if(i == 0){
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.lengaujes.html.lengauje}</h1>
+            <p>Manejo del lenguaje: ${habilidades.lengaujes.html.nivel}</p>
+            <ul>
+                <li>${habilidades.lengaujes.html.habilidades.layout}</li>
+                <li>${habilidades.lengaujes.html.habilidades.forms}</li>
+                <li>${habilidades.lengaujes.html.habilidades.dialog}</li>
+                <li>${habilidades.lengaujes.html.habilidades.table}</li>
+            </ul>`;
+        } else if (i == 1) {
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.lengaujes.css.lengauje}</h1>
+            <p>Manejo del lenguaje: ${habilidades.lengaujes.css.nivel}</p>
+            <ul>
+                <li>${habilidades.lengaujes.css.habilidades.grid}</li>
+                <li>${habilidades.lengaujes.css.habilidades.flex}</li>
+            </ul>
+            `;
+        } else if (i == 2){
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.lengaujes.javascript.lengauje}</h1>
+            <p>Manejo del lenguaje: ${habilidades.lengaujes.javascript.nivel}</p>
+            <ul>
+                <li>${habilidades.lengaujes.javascript.habilidades.dom}</li>
+                <li>${habilidades.lengaujes.javascript.habilidades.event}</li>
+                <li>${habilidades.lengaujes.javascript.habilidades.function}</li>
+                <li>${habilidades.lengaujes.javascript.habilidades.object}</li>
+            </ul>`;
+        } else if (i == 3) {
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.lengaujes.mysql.lengauje}</h1>
+            <p>Manejo del lenguaje: ${habilidades.lengaujes.mysql.nivel}</p>
+            <ul>
+                <li>${habilidades.lengaujes.mysql.habilidades.dbStruct}</li>
+                <li>${habilidades.lengaujes.mysql.habilidades.dbSPD}</li>
+                <li>${habilidades.lengaujes.mysql.habilidades.dbTransactional}</li>
+            </ul>`;
+        } else if(i == 4){
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.herramientas.git.herramienta}</h1>
+            <p>Manejo del lenguaje: ${habilidades.herramientas.git.nivel}</p>
+            <ul>
+                <li>${habilidades.herramientas.git.habilidades.init}</li>
+                <li>${habilidades.herramientas.git.habilidades.basic}</li>
+                <li>${habilidades.herramientas.git.habilidades.branch}</li>
+                <li>${habilidades.herramientas.git.habilidades.conflict}</li>
+            </ul>`;
+        } else if(i == 5){
+            displayDialog.innerHTML = `
+            <h1 class="title-info">Habilidades en ${habilidades.herramientas.github.herramienta}</h1>
+            <p>Manejo del lenguaje: ${habilidades.herramientas.git.nivel}</p>
+            <ul>
+                <li>${habilidades.herramientas.github.habilidades.repo}</li>
+                <li>${habilidades.herramientas.github.habilidades.merge}</li>
+            </ul>`;
+        }
+
+        const btnClose = document.createElement("button");
+        btnClose.classList.add("btn");
+        btnClose.textContent = "Cerrar";
+        displayDialog.appendChild(btnClose);
+        btnClose.addEventListener("click", function(){
+            displayDialog.close();
+        });
+    });
 });
-
-btnClose.addEventListener("click", function(){
-    displayDialog.close();
-});
-
-console.log(habilidades);
